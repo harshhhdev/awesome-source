f7f6f6cdf9f49543c833eede39b18dfd8e82cd64
Harsh Singh <harshhh.singh.personal@gmail.com>
2020-11-02 21:44:15 -0500

    Added login with Google
    I've replaced making an account and stuff with just a login with Google button. Much more simpler and not very time consuming also. and it sets up a lot of things already.
    
    Will be styling everything soon.

--- passport-config.js
+++ passport-config.js
@@ -1,30 +1,21 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth2').Strategy
 
-const LocalStrategy = require('passport-local').Strategy
-const bcrypt = require('bcrypt')
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
 
-function initialize(passport, getUserByEmail, getUserById) {
-  const authenticateUser = async (email, password, done) => {
-    const user = getUserByEmail(email)
-    if (user == null) {
-      return done(null, false, { message: 'No user with that email' })
-    }
-
-    try {
-      if (await bcrypt.compare(password, user.password)) {
-        return done(null, user)
-      } else {
-        return done(null, false, { message: 'Password incorrect' })
-      }
-    } catch (e) {
-      return done(e)
-    }
-  }
-
-  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
-  passport.serializeUser((user, done) => done(null, user.id))
-  passport.deserializeUser((id, done) => {
-    return done(null, getUserById(id))
-  })
+passport.use(new GoogleStrategy ({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    passReqToCallback: true
+}, function (request, accessToken, refreshToke, profile, done) {
+    console.log(profile)
+    return done(null, profile)
 }
-
-module.exports = initialize
+))
---
+++ server.js
@@ -0,0 +1,120 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+const Article = require('./models/article')
+require('./passport-config')
+
+app.use(express.static(__dirname + '/views/styling'))
+app.use(express.static(__dirname + '/views/articles/style'))
+app.use(express.static(__dirname + '/views/scripts'))
+
+mongoose.connect(process.env.MONGO_CONNECTION, {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+}, () => console.log('Connected to Database.'))
+  
+app.use(cookieSession({
+  name: 'tuto-session',
+  keys: ['key1', 'key2']
+}))
+
+app.set('view engine','ejs')
+
+const isLoggedIn = (req, res, next) => {
+  if (req.user) {
+      next();
+  } else {
+      res.redirect('/login')
+  }
+}
+
+// Initializes passport and passport sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Example protected and unprotected routes
+app.get('/', isLoggedIn, async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('index', { articles: articles, name: req.user.displayName, pic: req.user.photos[0].value, email: req.user.emails[0].value })
+})
+
+app.get('/failed', (req, res) => res.send('You Failed to log in!'))
+
+app.get('/', isLoggedIn, async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render("index",{ article: articles, name:req.user.displayName,pic:req.user.photos[0].value,email:req.user.emails[0].value})
+})
+
+// Auth Routes
+app.get('/google', passport.authenticate('google', { 
+  scope: ['profile', 'email'] 
+}));
+
+app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+app.get('/logout', (req, res) => {
+  req.session = null
+  req.logout()
+  res.redirect('/login')
+})
+  
+app.set('view-engine', 'ejs')
+  
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+  
+app.set('view engine', 'ejs')
+
+app.get('/new', isLoggedIn, (req, res) => {
+  res.render('articles/new', { article: new Article(), name: req.user.displayName })
+})
+
+app.get('/:slug', async (req, res) => {
+  const article = await Article.findOne({ slug: req.params.slug })
+  if (article == null) res.redirect('/')
+  res.render('articles/show', { article: article })
+})
+
+app.post('/', isLoggedIn, async (req, res, next) => {
+  console.log('Posting')
+  req.article = new Article()
+  next()
+}, saveArticleAndRedirect('new'))
+
+app.delete('/:id', async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id)
+  res.redirect('/')
+})
+
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    console.log('Working...')
+
+    let article = req.article
+
+    article.title = req.body.title
+    article.description = req.body.description
+    article.markdown = req.body.markdown
+    article.username = req.body.username
+    article.link = req.body.link
+    
+    try {
+      article = await article.save()
+      res.redirect(`/${article.slug}`)
+    } catch (e) {
+      console.log(e)
+      res.render(`/${path}`, { article: article })
+      console.log('Fail!')
+    }
+  }
+}
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Listening on ${port}!`))
